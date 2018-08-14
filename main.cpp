6f9f9c0060dfad44bf2e374e17fb7f808bec27a0
Estudiante <Estudiante@10.203.1.174>
2018-08-14 08:35:15 -0500

    LO primero para git

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: Estudiante
+ *
+ * Created on 14 de agosto de 2018, 08:30 AM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
